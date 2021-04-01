2720d8b5714686a18dacc07aa9fcd69c7783f274
PandaNah <pandanah@hotmail.com>
2021-04-01 12:31:33 +0300

    FTTT-1 Create env settings from .env

--- settings.py
+++ settings.py
@@ -1,3 +1,18 @@
 from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
+import typing
+
+
+@dataclass
+class GetEnv:
+    TELEGRAM_TOKEN: typing.AnyStr
+    ADMIN_LIST: typing.List[str]
+
 
 load_dotenv()
+envSettings = GetEnv(TELEGRAM_TOKEN=os.getenv('TELEGRAM_TOKEN'),
+                     ADMIN_LIST=os.getenv('ADMIN_LIST').split(', '))
+
+if __name__ == '__main__':
+    print(envSettings)
