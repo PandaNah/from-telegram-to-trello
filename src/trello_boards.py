93cf00f8e1a9519b52a863ecab2a0a30d2675d91
PandaNah <pandanah@hotmail.com>
2021-04-02 14:28:16 +0300

    Class for trello boards

--- src/trello_boards.py
+++ src/trello_boards.py
@@ -1,46 +1,21 @@
-import aiohttp
 import typing
+
 from settings import envSettings
-
-
-class TrelloBase:
-    def __init__(self):
-        self.url = "https://api.trello.com/1/"
-        self.headers = {'Accept': 'application/json'}
-        self.query = {'key': envSettings.TRELLO_KEY,
-                      'token': envSettings.TELEGRAM_TOKEN}
-
-    def get_url(self):
-        return self.url
-
-    def set_url(self, value: typing.Optional[str]):
-        self.url = self.url + value
-
-    def get_headers(self):
-        return self.headers
-
-    def set_headers(self, **kwargs):
-        for key, item in kwargs.items():
-            self.headers[key] = item
-
-    def get_query(self):
-        return self.query
-
-    def set_query(self, **kwargs):
-        for key, item in kwargs.items():
-            self.query[key] = item
+from src.trello_base import TrelloBase
+from src.trello_dataclasses import Membership
 
 
 class TrelloBoard(TrelloBase):
     def __init__(self):
         super().__init__()
-        self.url += f'boards/{envSettings.BOARD_ID}'
+        self.primary_url = f'boards/{envSettings.BOARD_ID}/'
+
+    def get_response(self, secondary_url: typing.Optional[str],):
+        return self._get_response(primary_url=self.primary_url,
+                                  secondary_url=secondary_url)
 
     def get_memberships(self):
-        response = requests.request(
-            method='GET',
-            url=self.url,
-            headers=self.headers,
-            params=self.query
-        )
-        return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+        response = self.get_response(secondary_url='memberships')
+        members: typing.List[Membership] = [Membership.parse_obj(user) for user in response]
+
+        return members
