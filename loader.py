9e3758111d2e622ecb94202523fced5e5213d3de
PandaNah <pandanah@hotmail.com>
2021-04-02 11:08:27 +0300

    new line in the end

--- loader.py
+++ loader.py
@@ -1,9 +1,9 @@
-from aiogram import Bot
+from aiogram import Bot, types
 from aiogram import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from settings import envSettings
 
-bot = Bot(token=envSettings.TELEGRAM_TOKEN)
+bot = Bot(token=envSettings.TELEGRAM_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
