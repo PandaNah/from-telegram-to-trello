72aa91eea702794ee5be6e99bd994a9eb7fbc108
PandaNah <pandanah@hotmail.com>
2021-04-02 11:11:55 +0300

    added trello key, token, board keys

--- settings.py
+++ settings.py
@@ -8,9 +8,17 @@ import typing
 class GetEnv:
     TELEGRAM_TOKEN: typing.AnyStr
     ADMIN_LIST: typing.List[str]
+    TRELLO_KEY: typing.AnyStr
+    TRELLO_TOKEN: typing.AnyStr
+    BOARD_ID: typing.AnyStr
 
 
 load_dotenv()
 envSettings = GetEnv(TELEGRAM_TOKEN=os.getenv('TELEGRAM_TOKEN'),
-                     ADMIN_LIST=os.getenv('ADMIN_LIST').split(', '))
+                     ADMIN_LIST=os.getenv('ADMIN_LIST').split(', '),
+                     TRELLO_KEY=os.getenv('TRELLO_KEY'),
+                     TRELLO_TOKEN=os.getenv('TRELLO_TOKEN'),
+                     BOARD_ID=os.getenv('BOARD_ID'),
+                     )
+
 
