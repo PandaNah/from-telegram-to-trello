5265e631f94a28951658d067793b3a815a53a195
PandaNah <pandanah@hotmail.com>
2021-04-02 14:28:52 +0300

    dataclasses for pydantic

--- src/trello_dataclasses.py
+++ src/trello_dataclasses.py
@@ -6,3 +6,9 @@ from pydantic import Field
 class Membership(BaseModel):
     id_member: typing.Optional[str] = Field(alias='idMember')
     member_type: typing.Optional[str] = Field(alias='memberType')
+
+
+class Member(BaseModel):
+    id_member: typing.Optional[str] = Field(alias='id')
+    username: typing.Optional[str]
+    full_name: typing.Optional[str] = Field(alias='fullName')
