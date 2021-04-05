af0e40844f6e4fddd07a9cf34986cf9a54966368
PandaNah <pandanah@hotmail.com>
2021-04-05 12:01:28 +0300

    FTTT-10 TrelloBase -> ABC

--- src/trello_base.py
+++ src/trello_base.py
@@ -1,33 +1,44 @@
 import typing
 import requests
+from abc import ABC
 from copy import deepcopy
 from settings import envSettings
+from collections import OrderedDict
 
 
-class TrelloBase:
+class TrelloBase(ABC):
     def __init__(self):
-        self.base_url = "https://api.trello.com/1/"
-        self.base_headers = {'Accept': 'application/json'}
-        self.base_query = {'key': envSettings.TRELLO_KEY,
-                           'token': envSettings.TRELLO_TOKEN}
+        self.base_url: typing.Final = "https://api.trello.com/1/"
+        self.base_headers: typing.Final = {'Accept': 'application/json'}
+        self.base_query: typing.Final = OrderedDict({'key': envSettings.TRELLO_KEY,
+                                                     'token': envSettings.TRELLO_TOKEN})
 
     def _get_response(self,
                       primary_url: str,
                       secondary_url: typing.Optional[str] = None,
-                      secondary_headers: typing.Optional[typing.Dict[str, str]] = None,
-                      secondary_params: typing.Optional[typing.Dict[str, str]] = None,
+                      secondary_params: typing.Optional[str] = None,
+                      is_headers: bool = True
                       ) -> dict:
+        """
+        Base function to make get response form trello
+
+        :param primary_url: Primary key
+        :param secondary_url: Secondary key
+        :param secondary_params: Additional params for query
+        :param is_headers: Sometimes no need in headers
+        :return: response from trello
+        """
 
         _final_url = deepcopy(self.base_url) + primary_url
-        _final_headers = deepcopy(self.base_headers)
+        _final_headers = None
         _final_params = deepcopy(self.base_query)
 
         if isinstance(secondary_url, str):
             _final_url += secondary_url
-        if isinstance(secondary_headers, dict):
-            _final_headers = {**self.base_headers, **secondary_headers}
-        if isinstance(secondary_params, dict):
-            _final_params = {**self.base_query, **secondary_params}
+        if is_headers:
+            _final_headers = deepcopy(self.base_headers)
+        if isinstance(secondary_params, str):
+            _final_params.update({'query': secondary_params})
 
         response = requests.request(method='GET',
                                     url=_final_url,
@@ -35,4 +46,3 @@ class TrelloBase:
                                     params=_final_params)
 
         return response.json()
-
