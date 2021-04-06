c0610fb1aaac5a711442081361c30882ebbcaaf5
PandaNah <pandanah@hotmail.com>
2021-04-06 13:03:57 +0300

    split settings on telegram set and trello set

--- settings.py
+++ settings.py
@@ -5,20 +5,27 @@ import typing
 
 
 @dataclass
-class GetEnv:
+class TelegramEnv:
     TELEGRAM_TOKEN: typing.AnyStr
     ADMIN_LIST: typing.List[str]
+
+
+@dataclass
+class TrelloEnv:
     TRELLO_KEY: typing.AnyStr
     TRELLO_TOKEN: typing.AnyStr
     BOARD_ID: typing.AnyStr
 
 
 load_dotenv()
-envSettings = GetEnv(TELEGRAM_TOKEN=os.getenv('TELEGRAM_TOKEN'),
-                     ADMIN_LIST=os.getenv('ADMIN_LIST').split(', '),
-                     TRELLO_KEY=os.getenv('TRELLO_KEY'),
-                     TRELLO_TOKEN=os.getenv('TRELLO_TOKEN'),
-                     BOARD_ID=os.getenv('BOARD_ID'),
-                     )
 
+envSettings = TelegramEnv(
+    TELEGRAM_TOKEN=os.getenv('TELEGRAM_TOKEN'),
+    ADMIN_LIST=os.getenv('ADMIN_LIST').split(', ')
+)
 
+trelloSettings = TrelloEnv(
+    TRELLO_KEY=os.getenv('TRELLO_KEY'),
+    TRELLO_TOKEN=os.getenv('TRELLO_TOKEN'),
+    BOARD_ID=os.getenv('BOARD_ID'),
+)
