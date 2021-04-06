5ac33e7ce9e47226fa469e1dd8e942658e047acb
PandaNah <pandanah@hotmail.com>
2021-04-06 13:06:25 +0300

    added docstrings and call_method

--- src/trello_lists.py
+++ src/trello_lists.py
@@ -6,11 +6,19 @@ from src.trello_dataclasses import BoardList
 class TrelloList(TrelloBase):
     def __init__(self):
         super().__init__()
+        """
+        Set primary url with 'lists/'
+        """
         self.primary_url = 'lists/'
 
     def get_list(self, list_id: typing.AnyStr) -> BoardList:
-        response = self._get_response(primary_url=self.primary_url,
-                                      secondary_url=list_id)
+        """
+        Get list from board by list_id
+
+        :param list_id: ID of list to search
+        :return: BoardList
+        """
+        response = self._make_response(call_method='GET', primary_url=self.primary_url, secondary_url=list_id)
 
         board_list: BoardList = BoardList.parse_obj(response)
 
