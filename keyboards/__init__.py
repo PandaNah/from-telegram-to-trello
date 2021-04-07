43a3b924cd9a0df45050f0df5dcc2baf5e7232b5
PandaNah <pandanah@hotmail.com>
2021-04-07 14:30:29 +0300

    init tags keyboard

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -3,3 +3,4 @@ from .new_task_keyboards import keyboard_with_members
 from .new_task_keyboards import keyboard_with_tags
 from .new_task_keyboards import keyboard_with_lists
 from .new_task_keyboards import keyboard_with_deadline
+from .new_task_keyboards import keyboard_with_position
