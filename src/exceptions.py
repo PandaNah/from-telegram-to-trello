113ac74325ebe9c4dd626539089b7efc8d21cde1
PandaNah <pandanah@hotmail.com>
2021-04-19 10:14:43 +0300

    rework base class

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,24 @@
 class WrongDateFormat(Exception):
     def __init__(self, date):
         super().__init__(f'Не верный формат даты : {date}')
+
+
+class WrongCallMethod(Exception):
+    def __init__(self, call_method):
+        super().__init__(
+            f'Call method should be '
+            f'GET, PUT, POST or DELETE '
+            f'not {call_method}',
+        )
+
+
+class UnAuthorized(Exception):
+    def __init__(self):
+        super().__init__(
+            'Wrong trello settings. Trello key or Trello token',
+        )
+
+
+class ResourceUnavailable(Exception):
+    def __init__(self):
+        super().__init__('Trello is unavailable now.')
